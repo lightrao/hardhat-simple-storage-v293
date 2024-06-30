4a9c93e8334c22d4f2941a4750ef4e50197f9394
lightrao <lightrao@hotmail.com>
2024-06-30 21:06:41 +0800

    update comments

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,46 +1,56 @@
-require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("./tasks/block-number")
-// require("@nomiclabs/hardhat-etherscan")
-require("dotenv").config()
-require("solidity-coverage")
-require("@nomicfoundation/hardhat-verify")
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+// Import Hardhat plugins and dependencies
+require("@nomiclabs/hardhat-waffle") // Plugin for integrating Waffle, a testing framework for Ethereum smart contracts
+require("hardhat-gas-reporter") // Plugin for generating gas usage reports
+require("./tasks/block-number") // Custom task definition for retrieving the current block number
+// require("@nomiclabs/hardhat-etherscan"); // Plugin for verifying contracts on Etherscan (commented out)
+require("dotenv").config() // Load environment variables from .env file
+require("solidity-coverage") // Plugin for generating test coverage reports for Solidity contracts
+require("@nomicfoundation/hardhat-verify") // Plugin for contract verification
+
+// Exporting an object to set up your Hardhat configuration
+// Learn more about configuration options at https://hardhat.org/config/
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+// Retrieve environment variables or set default values
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ""
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
+    // Set the default network to "hardhat"
     defaultNetwork: "hardhat",
+    // Define network configurations
     networks: {
-        hardhat: {},
+        hardhat: {}, // Default Hardhat local network configuration
         sepolia: {
-            url: SEPOLIA_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            chainId: 11155111,
+            // Configuration for the Sepolia testnet
+            url: SEPOLIA_RPC_URL, // RPC URL for connecting to Sepolia
+            accounts: [PRIVATE_KEY], // Array of private keys for deploying contracts
+            chainId: 11155111, // Chain ID for Sepolia
         },
         localhost: {
-            url: "http://localhost:8545",
-            // accounts: "hardhat supply",
-            chainId: 31337,
+            // Configuration for local Ethereum node
+            url: "http://localhost:8545", // Localhost RPC URL
+            // accounts: "hardhat supply", // Uncomment and provide accounts if needed
+            chainId: 31337, // Chain ID for local network
         },
     },
+    // Specify Solidity compiler version
     solidity: "0.8.8",
+    // Etherscan API key for contract verification
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
     },
+    // Configuration for gas reporter
     gasReporter: {
-        enabled: false, // close reporter
-        currency: "USD",
-        outputFile: "gas-report.txt",
-        noColors: true,
-        coinmarketcap: COINMARKETCAP_API_KEY,
-        // token: "MATIC", // default is ETH
+        enabled: false, // Disable gas reporter by default
+        currency: "USD", // Report gas costs in USD
+        outputFile: "gas-report.txt", // Output file for gas report
+        noColors: true, // Disable colors in gas report
+        coinmarketcap: COINMARKETCAP_API_KEY, // API key for CoinMarketCap to fetch gas prices
+        // token: "MATIC", // Uncomment and set token to report gas costs in MATIC (default is ETH)
     },
 }
--- test/test-deploy.js
+++ test/test-deploy.js
@@ -1,49 +1,63 @@
+// Import necessary modules from hardhat and chai
 const { ethers } = require("hardhat")
 const { expect, assert } = require("chai")
 
-// describe("SimpleStorage", () => {})
+// Describe the test suite for the SimpleStorage contract
 describe("SimpleStorage", function () {
-  // let simpleStorageFactory
-  // let simpleStorage
-  let simpleStorageFactory, simpleStorage
-  beforeEach(async function () {
-    simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
-    simpleStorage = await simpleStorageFactory.deploy()
-  })
+    // Declare variables to hold the contract factory and the deployed contract instance
+    let simpleStorageFactory, simpleStorage
 
-  it("Should start with a favorite number of 0", async function () {
-    const currentValue = await simpleStorage.retrieve()
-    const expectedValue = "0"
-    // assert
-    // expect
-    assert.equal(currentValue.toString(), expectedValue)
-    // expect(currentValue.toString()).to.equal(expectedValue)
-  })
-  it("Should update when we call store", async function () {
-    const expectedValue = "7"
-    const transactionResponse = await simpleStorage.store(expectedValue)
-    await transactionResponse.wait(1)
+    // Hook that runs before each test case
+    beforeEach(async function () {
+        // Get the contract factory for SimpleStorage
+        simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+        // Deploy a new instance of SimpleStorage
+        simpleStorage = await simpleStorageFactory.deploy()
+    })
 
-    const currentValue = await simpleStorage.retrieve()
-    assert.equal(currentValue.toString(), expectedValue)
-  })
+    // Test case to check the initial value of favorite number
+    it("Should start with a favorite number of 0", async function () {
+        // Retrieve the current value of favorite number from the contract
+        const currentValue = await simpleStorage.retrieve()
+        const expectedValue = "0"
 
-  // Extra - this is not in the video
-  it("Should work correctly with the people struct and array", async function () {
-    const expectedPersonName = "Patrick"
-    const expectedFavoriteNumber = "16"
-    const transactionResponse = await simpleStorage.addPerson(
-      expectedPersonName,
-      expectedFavoriteNumber
-    )
-    await transactionResponse.wait(1)
-    const { favoriteNumber, name } = await simpleStorage.people(0)
-    // We could also do it like this
-    // const person = await simpleStorage.people(0)
-    // const favNumber = person.favoriteNumber
-    // const pName = person.name
+        // Assert that the current value is equal to the expected value (0)
+        assert.equal(currentValue.toString(), expectedValue)
+        // Alternatively, you can use expect syntax
+        // expect(currentValue.toString()).to.equal(expectedValue);
+    })
 
-    assert.equal(name, expectedPersonName)
-    assert.equal(favoriteNumber, expectedFavoriteNumber)
-  })
+    // Test case to check the update functionality of store function
+    it("Should update when we call store", async function () {
+        const expectedValue = "7"
+        // Call the store function to update the favorite number
+        const transactionResponse = await simpleStorage.store(expectedValue)
+        // Wait for the transaction to be mined
+        await transactionResponse.wait(1)
+
+        // Retrieve the updated value of favorite number
+        const currentValue = await simpleStorage.retrieve()
+        // Assert that the updated value is equal to the expected value (7)
+        assert.equal(currentValue.toString(), expectedValue)
+    })
+
+    // Extra test case to check the functionality of people struct and array
+    it("Should work correctly with the people struct and array", async function () {
+        const expectedPersonName = "Patrick"
+        const expectedFavoriteNumber = "16"
+        // Call the addPerson function to add a new person to the contract
+        const transactionResponse = await simpleStorage.addPerson(
+            expectedPersonName,
+            expectedFavoriteNumber,
+        )
+        // Wait for the transaction to be mined
+        await transactionResponse.wait(1)
+
+        // Retrieve the first person from the people array
+        const { favoriteNumber, name } = await simpleStorage.people(0)
+
+        // Assert that the name and favorite number are equal to the expected values
+        assert.equal(name, expectedPersonName)
+        assert.equal(favoriteNumber, expectedFavoriteNumber)
+    })
 })
